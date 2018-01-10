df25125a8ec6c75b1a5dcd022ca6e9b8241120eb
JeffCabu <Hideki138@gmail.com>
2018-01-09 21:16:37 -0700

    server up

---
+++ server/routes/products.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const Router = express.Router()
+const Product = require('../models/Product')
+
+// its still "/api/products" because of server.js
+
+Router.route('/')
+  .get((req, res) => {
+    Product.find((err, products) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: 'Success', products})
+      }
+    })
+  })
+  .post((req, res) => {
+    const product = new Product()
+    product.setProductData(req.body)
+    product.setDate()
+    product.save((err, savedProduct) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: 'Success', data: savedProduct})
+      }
+    })
+  })
+
+Router.route('/:productId')
+  .get((req, res) => {
+    const productId = req.params.productId
+    Product.findById({_id: productId}, (err, product) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: `Found: ${productId}`, product})
+      }
+    })
+  })
+  .put((req, res) => {
+    const editProductId = req.params.productId
+    Product.findById({_id: editProductId}, (err, product) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        product.setProductData(req.body)
+        product.save((err, updatedProduct) => {
+          if (err) {
+            res.json({error: err})
+          } else {
+            res.json({msg: 'Successfully updated', product: updatedProduct})
+          }
+        })
+      }
+    })
+  })
+  .delete((req, res) => {
+    const deleteProduct = req.params.productId
+    Product.remove({_id: deleteProduct}, (err, product) => {
+      if (err) {
+        res.json({error: err})
+      } else {
+        res.json({msg: `Deleted: ${product}`})
+      }
+    })
+  })
+
+module.exports = Router
--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
+const productRoute = require('./routes/products')
 
 const port = 3001
 app.set('trust proxy', '127.0.0.1')
 
-mongoose.connect('mongodb://localhost/__YOUR_DB__')
+mongoose.connect('mongodb://localhost/e-commerce')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(require('./config/error-handler'))
+
+app.use('/api/products', productRoute)
 
 const server = app.listen(port, () => console.log(`Running on port: ${port}`))
 
